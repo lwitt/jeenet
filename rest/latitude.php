7a73412431dee258e7a5c9132f3225e638c24093
lwitt <laurent.witt@gmail.com>
2013-01-08 14:55:09 +0100

    Update rest/latitude.php

--- rest/latitude.php
+++ rest/latitude.php
@@ -4,8 +4,7 @@
   <?php
   $url = "https://latitude.google.com/latitude/apps/badge/api?user=7204587664094710976&type=json";
   		$data = utf8_encode(file_get_contents($url));
-      
-      echo $data;
+      var_dump(json_decode($json));
   ?>
 
 </body>
