947dac6dff6832bfbdee11c757ca37bfc71c98e0
lwitt <laurent.witt@gmail.com>
2013-01-08 14:31:18 +0100

    Update jsgraph3.php

--- jsgraph3.php
+++ jsgraph3.php
@@ -76,7 +76,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=jeenet', 'pi', 'mdpmdp');
 		new google.visualization.Gauge(document.getElementById('gauge2')). draw(data3,gauge2Options);
 
 
-		var data4 = google.visualization.arrayToDataTable([ ['x', '&deg;C'], 
+		var data4 = google.visualization.arrayToDataTable([ ['x', '\260C'], 
 
 		<?php
 			$date=$_GET['date'];
@@ -91,7 +91,7 @@ $bdd = new PDO('mysql:host=localhost;dbname=jeenet', 'pi', 'mdpmdp');
 		]); 
 
 		var options2 = {
-          		title: 'Température bureau <?php echo $date ?>',
+          		title: 'Temp\351rature bureau <?php echo $date ?>',
           		width: 1000,
 	   		height: 500,
 	  		vAxis : {maxValue:35}
